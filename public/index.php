17a8c0a7bfa7d5e020b6a019a57fea5b7a04ea47
Michael <notmichaelsu@gmail.com>
2025-03-22 05:48:39 -0700

    added nav bar, signin

--- public/index.php
+++ public/index.php
@@ -7,6 +7,23 @@
 
     <title>Marine Drive Rental</title>
 </head>
+<?php
+$isSignedIn = isset($_SESSION['user']); // Assuming session is used to track signed-in users
+?>
+<nav>
+    <ul style="list-style-type: none; margin: 0; padding: 0; overflow: hidden; background-color: #333;">
+        <li style="float: left;">
+            <a href="index.php" style="display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;">Home</a>
+        </li>
+        <li style="float: right;">
+            <?php if ($isSignedIn): ?>
+                <a href="profile.php" style="display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;">Profile</a>
+            <?php else: ?>
+                <a href="signin.php" style="display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;">Sign In</a>
+            <?php endif; ?>
+        </li>
+    </ul>
+</nav>
 
 <body>
     <h1>Welcome to Marine Drive Rental</h1>
--- public/reserve.php
+++ public/reserve.php
@@ -46,6 +46,17 @@ $car = mysqli_fetch_assoc($result);
     <title>Browse Cars</title>
     <link rel="stylesheet" href="../css/styles.css">
 </head>
+<nav>
+    <ul style="list-style-type: none; margin: 0; padding: 0; overflow: hidden; background-color: #333;">
+        <li style="float: left;">
+            <a href="index.php" style="display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;">Home</a>
+        </li>
+
+        <li style="float: right;">
+            <a href="signin.php" style="display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;">Sign In</a>
+        </li>
+    </ul>
+</nav>
 
 <body>
 
